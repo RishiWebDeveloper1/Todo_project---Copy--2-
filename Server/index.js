01f07347df961442c31b58780bb70dc4b2bca154
Rishi.R.Vishwakarma <149783805+RishiWebDeveloper1@users.noreply.github.com>
2024-09-04 16:45:09 +0530

    update index.js

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ const TodoModel = require("./Models/Todo")
 const app = express();
 app.use(cors(
     {
-        origin: [""],
+        origin: ["https://todo-project-copy-2.vercel.app/"],
         methods: ["POST", "GET", "PUT"],
         credentials: true
     }
