9bb5a217b79e30bc13156dc342abedf6a642401f
Rishi.R.Vishwakarma <149783805+RishiWebDeveloper1@users.noreply.github.com>
2024-09-04 15:31:47 +0530

    update index.js hii

--- Server/index.js
+++ Server/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 mongoose.connect('mongodb://127.0.0.1:27017/Todo')
 
 app.get('/', (req, res) => {
-        res.json("hello")
+        res.json("hello hii")
 })
 
 app.get('/get', (req, res) => {
