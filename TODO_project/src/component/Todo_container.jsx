55c63c9b06113d1192bf46613e914d917fa9f3da
Rishi.R.Vishwakarma <149783805+RishiWebDeveloper1@users.noreply.github.com>
2024-09-04 18:00:44 +0530

    Update Todo_container.jsx

--- TODO_project/src/component/Todo_container.jsx
+++ TODO_project/src/component/Todo_container.jsx
@@ -52,10 +52,10 @@ const Todo_container = ({ todos, fetchTodo }) => {
                         }
                         <div className="img-box">
                             <div className="edit-icon-box">
-                                <img src="src\assets\edit-icon.gif" alt="edit" onClick={() => startEdit(todo._id, todo.task)} className="edit-icon" />
+                                <img src="./src\assets\edit-icon.gif" alt="edit" onClick={() => startEdit(todo._id, todo.task)} className="edit-icon" />
                             </div>
                             <div className="delete-icon-box">
-                                <img src="src\assets\delete-icon.gif" alt="delete" onClick={() => deleteText(todo._id)} className="delete-icon" />
+                                <img src="./src\assets\delete-icon.gif" alt="delete" onClick={() => deleteText(todo._id)} className="delete-icon" />
                             </div>
                         </div>
                     </div>
