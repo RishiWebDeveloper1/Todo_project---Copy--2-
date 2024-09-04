bfda7e4aa77287d4ccb670a1ea6855ab3f47e408
Rishi.R.Vishwakarma <149783805+RishiWebDeveloper1@users.noreply.github.com>
2024-09-04 19:22:25 +0530

    Update App.jsx

--- TODO_project/src/App.jsx
+++ TODO_project/src/App.jsx
@@ -1,21 +1,31 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import Navbar from './component/Navbar'
-import Search_panel from './component/Search_panel'
-import Todo_container from './component/Todo_container'
-import axios from 'axios'
+import { useState, useEffect } from 'react';
+import './App.css';
+import Navbar from './component/Navbar';
+import Search_panel from './component/Search_panel';
+import Todo_container from './component/Todo_container';
+import axios from 'axios';
+import { io } from 'socket.io-client';
+
+const socket = io('https://todo-master-server.vercel.app');
 
 function App() {
     const [todos, setTodo] = useState([]);
 
     useEffect(() => {
-        fetchTodo()
-    }, [])
+        fetchTodo();
+
+        socket.on('todoUpdated', () => {
+            fetchTodo();
+        });
+        return () => {
+            socket.off('todoUpdated');
+        };
+    }, []);
 
     function fetchTodo() {
         axios.get('https://todo-master-server.vercel.app/get')
             .then(result => setTodo(result.data))
-            .catch(err => console.log(err))
+            .catch(err => console.log(err));
     }
 
     return (
@@ -24,7 +34,7 @@ function App() {
             <Search_panel fetchTodo={fetchTodo} />
             <Todo_container todos={todos} fetchTodo={fetchTodo} />
         </>
-    )
+    );
 }
 
-export default App
+export default App;
