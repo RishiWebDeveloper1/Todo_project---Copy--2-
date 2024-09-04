d9190acb07fd46b89723fbf21e073e26a3db1db1
Rishi.R.Vishwakarma <149783805+RishiWebDeveloper1@users.noreply.github.com>
2024-09-04 20:29:19 +0530

    undo updated code

--- Server/index.js
+++ Server/index.js
@@ -1,76 +1,43 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const TodoModel = require("./Models/Todo");
-const http = require("http");
-const { Server } = require("socket.io");
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const TodoModel = require("./Models/Todo")
+
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "https://todo-masters.vercel.app",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true
-    }
-});
-const serverio = new Server(server, {
-    cors: {
-        origin: "*"
-    }
-});
+app.use(cors())
+app.use(express.json())
 
-app.use(cors());
-app.use(express.json());
-
-mongoose.connect('mongodb+srv://rishivishwa4877:rishiMongodb@cluster0.k16x7.mongodb.net/Todo?retryWrites=true&w=majority&appName=Cluster0', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch(err => console.error("Failed to connect to MongoDB", err));
-
-app.get('/', (req, res) => {
-    res.json("hello hii");
-});
+mongoose.connect('mongodb://127.0.0.1:27017/Todo')
 
 app.get('/get', (req, res) => {
     TodoModel.find()
         .then(result => res.json(result))
-        .catch(err => res.json(err));
-});
+        .catch(err => res.json(err))
+})
 
 app.post('/add', (req, res) => {
     const task = req.body.task;
-    TodoModel.create({ task: task }).then(result => {
-        server.emit('todoUpdated');  // Notify all clients about the update
-        res.json(result);
-    })
-    .catch(err => res.json(err));
-});
+    TodoModel.create({ task: task }).then(result => res.json(result))
+        .catch(err => res.json(err))
+})
 
 app.delete('/delete/:id', (req, res) => {
     const { id } = req.params;
     TodoModel.findByIdAndDelete({ _id: id })
-        .then(result => {
-            server.emit('todoUpdated');  // Notify all clients about the update
-            res.json(result);
-        })
-        .catch(err => res.json(err));
-});
+        .then(result => { res.json(result) })
+        .catch(err => { res.json(err) })
+})
 
 app.put('/update/:id', (req, res) => {
     const { id } = req.params;
     const { task } = req.body;
 
     TodoModel.findByIdAndUpdate(id, { task: task }, { new: true })
-        .then(result => {
-            server.emit('todoUpdated');  // Notify all clients about the update
-            res.json(result);
-        })
+        .then(result => res.json(result))
         .catch(err => res.json(err));
 });
 
-server.listen(3000, () => {
-    console.log("Server is Started....");
-});
+app.listen(3000, () => {
+    console.log("Server is Started....")
+})
--- TODO_project/src/App.jsx
+++ TODO_project/src/App.jsx
@@ -1,32 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { io } from 'socket.io-client';
-import './App.css';
-import Navbar from './component/Navbar';
-import Search_panel from './component/Search_panel';
-import Todo_container from './component/Todo_container';
-
-const socket = io('https://todo-master-server.vercel.app'); // Connect to your Vercel server
+import { useState, useEffect } from 'react'
+import './App.css'
+import Navbar from './component/Navbar'
+import Search_panel from './component/Search_panel'
+import Todo_container from './component/Todo_container'
+import axios from 'axios'
 
 function App() {
     const [todos, setTodo] = useState([]);
 
     useEffect(() => {
-        fetchTodo();
-
-        socket.on('todoUpdated', () => {
-            fetchTodo();  // Refetch todos when an update is detected
-        });
-
-        return () => {
-            socket.off('todoUpdated');  // Clean up the event listener
-        };
-    }, []);
+        fetchTodo()
+    }, [])
 
     function fetchTodo() {
-        axios.get('https://todo-master-server.vercel.app/get')
+        axios.get('http://localhost:3000/get')
             .then(result => setTodo(result.data))
-            .catch(err => console.log(err));
+            .catch(err => console.log(err))
     }
 
     return (
@@ -35,7 +24,7 @@ function App() {
             <Search_panel fetchTodo={fetchTodo} />
             <Todo_container todos={todos} fetchTodo={fetchTodo} />
         </>
-    );
+    )
 }
 
-export default App;
+export default App
