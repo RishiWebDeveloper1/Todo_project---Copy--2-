4710b26f8ed8b87cd14fe931006fd1b8826eacda
Rishi.R.Vishwakarma <149783805+RishiWebDeveloper1@users.noreply.github.com>
2024-09-04 19:22:02 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,23 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const TodoModel = require("./Models/Todo")
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const TodoModel = require("./Models/Todo");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
-app.use(cors(
-    {
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
         origin: ["https://todo-masters.vercel.app"],
         methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
-))
-app.use(express.json())
+});
 
-// mongoose.connect('mongodb://127.0.0.1:27017/Todo')
+app.use(cors());
+app.use(express.json());
+
 mongoose.connect('mongodb+srv://rishivishwa4877:rishiMongodb@cluster0.k16x7.mongodb.net/Todo?retryWrites=true&w=majority&appName=Cluster0', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -22,39 +25,47 @@ mongoose.connect('mongodb+srv://rishivishwa4877:rishiMongodb@cluster0.k16x7.mong
 .then(() => console.log("Connected to MongoDB"))
 .catch(err => console.error("Failed to connect to MongoDB", err));
 
-
 app.get('/', (req, res) => {
-        res.json("hello hii")
-})
+    res.json("hello hii");
+});
 
 app.get('/get', (req, res) => {
     TodoModel.find()
         .then(result => res.json(result))
-        .catch(err => res.json(err))
-})
+        .catch(err => res.json(err));
+});
 
 app.post('/add', (req, res) => {
     const task = req.body.task;
-    TodoModel.create({ task: task }).then(result => res.json(result))
-        .catch(err => res.json(err))
-})
+    TodoModel.create({ task: task }).then(result => {
+        io.emit('todoUpdated');
+        res.json(result);
+    })
+    .catch(err => res.json(err));
+});
 
 app.delete('/delete/:id', (req, res) => {
     const { id } = req.params;
     TodoModel.findByIdAndDelete({ _id: id })
-        .then(result => { res.json(result) })
-        .catch(err => { res.json(err) })
-})
+        .then(result => {
+            io.emit('todoUpdated');
+            res.json(result);
+        })
+        .catch(err => res.json(err));
+});
 
 app.put('/update/:id', (req, res) => {
     const { id } = req.params;
     const { task } = req.body;
 
     TodoModel.findByIdAndUpdate(id, { task: task }, { new: true })
-        .then(result => res.json(result))
+        .then(result => {
+            io.emit('todoUpdated');
+            res.json(result);
+        })
         .catch(err => res.json(err));
 });
 
-app.listen(3000, () => {
-    console.log("Server is Started....")
-})
+server.listen(3000, () => {
+    console.log("Server is Started....");
+});
